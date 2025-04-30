7e093e83f9d3fd67a3ce84227a29251e4f045a41
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-04-30 21:22:41 +0200

    fix(): check user and password for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -80,8 +80,7 @@ export const login = catchAsync(
     // 2) Check if user exists && password is correct
     const user = await User.findOne({ email }).select("+password");
 
-    // !(await user.correctPassword(password, user.password))
-    if (!user) {
+    if (!user || !(await user.correctPassword(password, user.password))) {
       return next(new AppError("Incorrect email or password", 401));
     }
 
